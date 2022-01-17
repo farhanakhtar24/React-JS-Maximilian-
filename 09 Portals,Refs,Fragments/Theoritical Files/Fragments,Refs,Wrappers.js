b46cdbdeb967f11ef06639f04faf5a9cd632fc48
Mohd Farhan Akhtar <akhtar.farhan779@gmail.com>
2022-01-17 13:30:03 +0530

    done

---
+++ 09 Portals,Refs,Fragments/Theoritical Files/Fragments,Refs,Wrappers.js
@@ -0,0 +1,6 @@
+/*
+React can use React fragments , custom wrappers or Refs and they basically just render {props.children} in the DOM
+and this helps us to remove the use of divs as wrappers on our content while writing the individual components
+
+If we use the divs as wrappers approach then they can lead to unnecessary creation of a lot of divs 
+*/
